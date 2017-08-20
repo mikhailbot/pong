5bd9f66acc0548b4990b48abc439aa3543e95ee6
Mikhail Delport <mikhail@delport.ca>
2017-08-20 15:10:50 -0400

    Update email tests

--- test/pong/mailer/mailer_test.exs
+++ test/pong/mailer/mailer_test.exs
@@ -4,18 +4,32 @@ defmodule Pong.MailerTest do
 
   use ExUnit.Case
   use Bamboo.Test
+  use Pong.DataCase
 
-  alias Pong.Mailer
+  alias Pong.{Mailer, Monitors}
+
+  @valid_attrs %{ip_address: "8.8.8.8", name: "some name"}
+
+  def host_fixture(attrs \\ %{}) do
+    {:ok, host} =
+      attrs
+      |> Enum.into(@valid_attrs)
+      |> Monitors.create_host()
+
+    host
+  end
 
   test "down notice is sent" do
-    email = Mailer.send_down_notice
+    host = host_fixture()
+    email = Mailer.send_down_notice(host)
 
-    assert email.html_body =~ "Something went down"
+    assert email.html_body =~ "#{host.name} is down"
   end
 
   test "up notice is sent" do
-    email = Mailer.send_up_notice
+    host = host_fixture()
+    email = Mailer.send_up_notice(host)
 
-    assert email.html_body =~ "Something came up"
+    assert email.html_body =~ "#{host.name} is back up"
   end
 end
